da4de23b0781053f794032422d9cc56e50ec51f9
Dominik Sobarnia <dominik.sobarnia@gmail.com>
2020-12-04 10:44:06 +0100

    7.1 Added text beautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,42 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.stream.Collectors;
+
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("fabulous and magnificient", text -> text.toUpperCase());
+        poemBeautifier.beautify("fabulous and magnificient", text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("fabulous and magnificient", text -> "****" + text + "****");
+        poemBeautifier.beautify(" fabulous and magnificient ", text -> text.replace(" ", "^"));
+        poemBeautifier.beautify("fabulous and magnificient", text -> text.chars()
+                .mapToObj(i -> Character.toString((char) i)).collect(Collectors.joining("|")));
+
+
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        System.out.println("Calculating expressions with lambdas");
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+//
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+
+    }
+}
